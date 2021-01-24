8364cae25841ae9caada005d9821a8249209b6c7
Leon Brooks <leon-brooks@web.de>
2021-01-25 00:34:40 +0100

    - added text to readme - added input/output examples - added -help printout to main

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Inverse-Transparency-NLG
+								Input format:
+.txt file with tuples of requesting user and issued request seperated by { character
+	
+	John{https://jira.atlassian.com/rest/api/latest/search?jql=creator = Peter
+
+- the search query must contain spaces between operators and fields
+- one request per line
+- empty lines may be placed inbetween requests
+
+
+
+								Output format:
+.txt file
+
+
+
+							     Program Parameters:
+- path to input file
+- path to output file
+- the time frame for which this program is run (e.g.: week, 12 days, month, etc.)
+- threshold of request issued by a single user which are considered not noteworthy (must be int)
+- threshold of request targeting a single user which are considered not noteworthy (must be int)
+- [option for detailed output, if wished give "detailed" without quotation marks as last parameter]
+
+
+
+The folder InOut contains an input and output example
--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,30 @@ public class Main {
         boolean detailed = false;
         try {
             if(args[0].compareToIgnoreCase("-help") == 0){
-                System.out.println("TODO help");
+                System.out.println("""
+                                      								Input format:
+                                      .txt file with tuples of requesting user and issued request seperated by { character
+                                      	
+                                      	John{https://jira.atlassian.com/rest/api/latest/search?jql=creator = Peter
+                                      
+                                      - the search query must contain spaces between operators and fields
+                                      - one request per line
+                                      - empty lines may be placed inbetween requests
+                                      
+                                      
+                                      
+                                      								Output format:
+                                      .txt file
+                                      
+                                      
+                                      
+                                      							   Program Parameters:
+                                      - path to input file
+                                      - path to output file
+                                      - the time frame for which this program is run (e.g.: week, 12 days, month, etc.)
+                                      - threshold of request issued by a single user which are considered not noteworthy (must be int)
+                                      - threshold of request targeting a single user which are considered not noteworthy (must be int)
+                                      - [option for detailed output, if wished give "detailed" without quotation marks as last parameter]""");
                 return;
             }
 
